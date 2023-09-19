738d9a1dce5aea7876328aaad4478675d8f9401e
Katia Nartovich <56835998+knartovich@users.noreply.github.com>
2023-09-19 14:24:54 -0500

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,7 +2,7 @@ with source as (
 
     {#-
     Normally we would select from the table here, but we are using seeds to load
-    our data in this projectsdafgdsasdf
+    our data in this projectsdafgdf
     #}
     select * from {{ ref('raw_customers') }}
 
